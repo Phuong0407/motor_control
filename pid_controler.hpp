f8ff0d91facdaf740838b33447f35068b4c78da3
Phuong0407 <diepthanhphuong0407@gmail.com>
2025-04-18 16:04:50 +0200

    add first raw pid control algo

---
+++ pid_controler.hpp
@@ -0,0 +1,45 @@
+/**
+ * @brief MotorUnit — Controls a motor via Grove I2C Motor Driver and measures RPM with encoder (DG01D-E)
+ * @file encoder.hpp
+ * @date April 18, 2025
+ * 
+ */
+
+#ifndef PID_CONTROLER_HPP
+#define PID_CONTROLER_HPP
+
+
+/**
+ * @brief PIDController class for implementing a PID control algorithm.
+ * 
+ */
+
+class PIDController {
+private:
+    double kp;
+    double ki;
+    double kd;
+
+    double setpoint;
+    double integral;
+    double previous_error;
+
+public:
+    PIDController(double kp, double ki, double kd)
+    : kp(kp), ki(ki), kd(kd), setpoint(0), integral(0), previous_error(0) {}
+
+    void setSetpoint(double new_setpoint) {
+        setpoint = new_setpoint;
+    }
+
+    double compute(double measured_value, double dt) {
+        double error = setpoint - measured_value;
+        integral += error * dt;
+        double derivative = (error - previous_error) / dt;
+        previous_error = error;
+        return kp * error + ki * integral + kd * derivative;
+    }
+};
+
+
+#endif // PID_CONTROLER_HPP
