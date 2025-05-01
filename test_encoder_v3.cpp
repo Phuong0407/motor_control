ca1caff7caeccd87b3892a6de9633480094e3544
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-02 01:01:23 +0200

    add cmd input

--- test_encoder_v3.cpp
+++ test_encoder_v3.cpp
@@ -1,16 +1,23 @@
 #include "encoder.hpp"
 
-void setMotorCommand(int ms = 1000) {
+#include <iostream>
+#include <string>
+
+void setMotorCommand(int ms = 1000, int speed = 0xffff) {
     int i2c_fd = wiringPiI2CSetup(0x0f);
-    wiringPiI2CWriteReg16(i2c_fd, 0x82, 0xffff);
+    wiringPiI2CWriteReg16(i2c_fd, 0x82, speed);
     wiringPiI2CWriteReg16(i2c_fd, 0xaa, 0x06);
     delay(ms);
     wiringPiI2CWriteReg16(i2c_fd, 0x82, 0x0000);
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    int ms = std::stoi(argv[1]);
+    int speed = std::stoi(argv[2]);
+
     EncoderManager encoder_manager;
-    setMotorCommand();
+    setMotorCommand(ms, speed);
+
     std::cout << "After run motor" << "\n";
     std::cout << "ENCODER COUNTER 1 = " << encoder1->getCounter() << "\n";
     std::cout << "ENCODER COUNTER 2 = " << encoder2->getCounter() << "\n";
