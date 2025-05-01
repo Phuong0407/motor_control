9ed5523f34b9c3f33477a2ab1a82ac23d1e649d0
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-02 00:48:37 +0200

    modify encoder test

--- test_encoder_v3.cpp
+++ test_encoder_v3.cpp
@@ -2,13 +2,9 @@
 
 void setMotorCommand() {
     int i2c_fd = wiringPiI2CSetup(0x0f);
-    int counter = 0;
-    while (counter <= 50) {
-        wiringPiI2CWriteReg16(i2c_fd, 0x82, 0xffff);
-        wiringPiI2CWriteReg16(i2c_fd, 0xaa, 0x06);
-        delay(100);
-        counter++;
-    }
+    wiringPiI2CWriteReg16(i2c_fd, 0x82, 0xffff);
+    wiringPiI2CWriteReg16(i2c_fd, 0xaa, 0x06);
+    delay(100);
     wiringPiI2CWriteReg16(i2c_fd, 0x82, 0x0000);
 }
 
