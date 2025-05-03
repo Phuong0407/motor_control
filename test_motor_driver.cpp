2954c923ffecd9176f861503568e21ac01f900dd
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-03 17:13:38 +0200

    refine code

--- test_motor_driver.cpp
+++ test_motor_driver.cpp
@@ -4,16 +4,16 @@ int main(int argc, char* argv[]) {
     if (argc < 5)
         return 0;
     
-    int pwm1        = std::stoi(argv[1], nullptr, 0);
-    int pwm2        = std::stoi(argv[2], nullptr, 0);
-    int pwm3        = std::stoi(argv[3], nullptr, 0);
+    double rps1     = std::stod(argv[1]);
+    double rps2     = std::stod(argv[2]);
+    double rps3     = std::stod(argv[3]);
     double run_itv  = std::stod(argv[4]);
     double smpl_itv = std::stod(argv[5]);
 
     MotorDriver motor_driver(0.05, 0.05, 0.05, 0.2, 0x0f, 0x0d);
 
     auto t_start = std::chrono::steady_clock::now();
-    motor_driver.setLeftRightMotor(0.2, 0.2, false);
+    motor_driver.setLeftRightMotor(0.6, 0.6, false);
     while (true) {
         auto t_now = std::chrono::steady_clock::now();
         std::chrono::duration<double> elapsed = t_now - t_start;
