359bf13a9b6a583f632666a80ef321a97698df15
Phuong0407 <diepthanhphuong0407@gmail.com>
2025-05-04 12:32:08 +0200

    .

--- test_encoder.cpp
+++ test_encoder.cpp
@@ -6,11 +6,11 @@
 #include <iostream>
 #include <string>
 
-void setMotorCommand(int ms = 10000, int speed = 0xffff) {
+void setMotorCommand(int ms = 200, int speed = 0xffff) {
     int i2c_fd1 = wiringPiI2CSetup(0x0f);
     int i2c_fd2 = wiringPiI2CSetup(0x0d);
 
-    wiringPiI2CWriteReg16(i2c_fd1, 0x82, 0xffff);
+    wiringPiI2CWriteReg16(i2c_fd1, 0x82, 0x4646);
     wiringPiI2CWriteReg16(i2c_fd1, 0xaa, 0x06);
     wiringPiI2CWriteReg16(i2c_fd2, 0x82, 0xffff);
     wiringPiI2CWriteReg16(i2c_fd2, 0xaa, 0x06);
