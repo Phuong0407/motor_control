7d206e200c1c6742aecd325baed0487836b22474
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-08 19:41:10 +0200

    add test for extract path

--- test/test_binarymask.cpp
+++ test/test_binarymask.cpp
@@ -2,13 +2,23 @@
 
 int main() {
     Vision vision(800, 640, 30, false);
+
     cv::Mat binary_mask;
+    cv::Mat pathImage;
+    std::vector<cv::Point> waypoints;
+
     int timeout = 1000;
     int ch = 0;
     while (ch != 27) {
         vision.extrackRouteBinaryMap(binary_mask, timeout);
+        vision.extractPath(binary_mask, pathImage, waypoints);
         cv::imshow("Binary Mask", binary_mask);
+        cv::imshow("Binary Mask", binary_mask);
+        cv::imshow("Path with Centerline", pathImage);
+        std::cout << "Waypoints:" << std::endl;
+        for (const auto &wp : waypoints) {
+            std::cout << "(" << wp.x << ", " << wp.y << ")" << std::endl;
+        }
         cv::waitKey(500);
     }
-
 }
