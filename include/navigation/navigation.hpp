e0466e4324440e6c87cd10957bc1388560f6f629
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-09 23:32:01 +0200

    .

--- include/navigation/navigation.hpp
+++ include/navigation/navigation.hpp
@@ -62,13 +62,13 @@ public:
             double contourX = vision.getCentroidXFirstSlices();
             cv::imshow("Raw", image1);
             cv::imshow("Processed", image2);
+            ch = cv::waitKey(5);
             printf("x = %f", contourX);
             double vy = kp_vy * (contourX - cam_offset);
     
             double omega1, omega2, omega3;
             kinemator.computeWheelVelocityFromRobotVelocity(vx, vy, 0.0, omega1, omega2, omega3);
-            // motor.controlAngularVelocity(omega1, omega2, omega3);
-            ch = cv::waitKey(5);
+            motor.controlAngularVelocity(omega1, omega2, omega3);
         }
     }
 };
