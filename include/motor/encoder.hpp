d2e503d387c62f6b4a545de8f27a630fc3245d38
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-10 03:37:48 +0200

    update encoder

--- include/motor/encoder.hpp
+++ include/motor/encoder.hpp
@@ -13,41 +13,51 @@
 constexpr int NUM_ENCODERS              = 3;
 static constexpr double COUNTER_PER_REV = 144.0;
 static constexpr double ANGLE_PER_TICK  = 2.5;
+static constexpr uint32_t DEBOUNCE_TIME_US = 828;
 
 class MotorEncoder {
 private:
     int H1, H2;
-    volatile int64_t counter;
+    volatile int64_t counter, last_time;
 
     void initEncoder(int i2c_addr) {
-        if (wiringPiSetup() == -1)
-            throw std::runtime_error("[ERROR] wiringPiSetup failed");
+        if (wiringPiSetup() == -1) {
+            printf("[ERROR] wiringPiSetup failed.\n");
+            return;
+        }
 
         int i2c_fd = wiringPiI2CSetup(i2c_addr);
-        if (i2c_fd < 0)
-            throw std::runtime_error("[ERROR] I2C setup failed");
+        if (i2c_fd < 0) {
+            printf("[ERROR] I2C setup failed.\n");
+        }
 
         pinMode(H1, INPUT);
         pinMode(H2, INPUT);
         pullUpDnControl(H1, PUD_UP);
         pullUpDnControl(H2, PUD_UP);
 
-        if (wiringPiI2CRead(i2c_fd) == -1)
-            std::cerr << "[ERROR] I2C device is not responding.\n";
-
-        std::cout   << "[INFO] Encoder initialized on pins H1 = "
-                    << H1 << ", H2 = " << H2 << "\n";
+        if (wiringPiI2CRead(i2c_fd) == -1) {
+            printf("[ERROR] I2C device is not responding.\n");
+            return;
+        }
+        printf("[INFO] Encoder initialized on pins H1 = %d, H2 = %d.\n", H1, H2);
     }
 
 public:
     MotorEncoder(int H1, int H2, int i2c_addr = 0x0f) :
-    H1(H1), H2(H2), counter(0)
+    H1(H1), H2(H2), counter(0), last_time(0)
     {
         initEncoder(i2c_addr);
     }
 
+
     void updateCounter() {
-        digitalRead(H2) ? ++counter : --counter;
+        volatile int64_t current_time = micros();
+        if ((current_time - last_time) >= DEBOUNCE_TIME_US) {
+            bool H2_STATE = digitalRead(H2);
+            counter += (H2_STATE ? 1 : -1); 
+            last_time = current_time;
+        }
     }
 
     void resetCounter() {
@@ -69,9 +79,9 @@ static IsrFunc isr_functions[NUM_ENCODERS] = {
 };
 
 static const int encoder_pin_table[NUM_ENCODERS][2] = {
-    {21, 22}, // D5  = {5 , 6 }
-    {3 , 4 }, // D22 = {22, 23}
-    {27, 0 }, // D16 = {16, 17}
+    {21, 22},
+    {3 , 4 },
+    {27, 0 },
 };
 
 inline void declareEncoders(int driver1_addr, int driver2_addr) {
@@ -93,9 +103,11 @@ inline void cleanupEncoders() {
 inline void attachEncoderInterrupts() {
     for (std::size_t i = 0; i < NUM_ENCODERS; ++i) {
         int H1 = encoder_pin_table[i][0];
-        if (wiringPiISR(H1, INT_EDGE_RISING, isr_functions[i]) < 0)
-            throw std::runtime_error("[ERROR] Failed to attach ISR to pin " + std::to_string(H1));
-        std::cout << "[INFO] ISR attached to pin " << H1 << "\n";
+        if (wiringPiISR(H1, INT_EDGE_RISING, isr_functions[i]) < 0) {
+            printf("[ERROR] Failed to attach ISR to pin %d.\n", H1);
+            return;
+        }
+        printf("[INFO] ISR attached to pin %d\n", H1);
     }
 }
 
