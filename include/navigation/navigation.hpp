4b0b7b1ed3a4e4f62c8df0ff502cae733a19ae84
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-10 04:14:06 +0200

    update navigation algo

--- include/navigation/navigation.hpp
+++ include/navigation/navigation.hpp
@@ -2,79 +2,82 @@
 #define NAVIGATION_HPP
 
 #include "vision.hpp"
-#include "orientation_pid.hpp"
 #include "kinematic_model.hpp"
 #include "../motor/motor.hpp"
-
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <vector>
+#include <numeric>
 
-class Navigation{
+class Navigation {
 private:
     double vx;
+    double kp_vy;
+    double kp_omega;
     double cam_offset;
-    double kp_vy, kp_omega;
-    
+
     Vision vision;
-    // OrientationPID orientation_y;
-    // OrientationPID orientation_omega;
     KinematicModel kinemator;
     MotorDriver motor;
 
 public:
     Navigation(
-        double vx,
-        double kp_vy,
-        // double kp_omega,
         double L1,
         double L2,
-        double r,
-        double cam_offset
+        double whl_r,
+        double cam_offset,
+        
+        double vx,
+        double kp_vy,
+        double kp_omega,
     ) :
     vx(vx),
-    cam_offset(cam_offset),
     kp_vy(kp_vy),
-    kp_omega(0.0),
-    // orientation_y(kp_vy, 0.0, 0.0, 10.0, 2.0),
-    // orientation_omega(kp_omega, 0.0, 0.0, 10.0, 2.0),
-    kinemator(L1, L2, r),
+    kp_omega(kp_omega),
+    cam_offset(cam_offset),
+    kinemator(L1, L2, whl_r),
     motor(2.0, 0.01, 0.5, 0.1, 0.7 * 0.5 / 0.1, 0x0f, 0x0d)
-    {
-        vision;
+    {}
+
+    double calculateAverageCentroidX(const std::vector<cv::Point>& centroids) {
+        if (centroids.empty()) return cam_offset;
+        double sum_x = std::accumulate(centroids.begin(), centroids.end(), 0.0,
+                                       [](double sum, const cv::Point& pt) {
+                                           return sum + pt.x;
+                                       });
+        return sum_x / centroids.size();
     }
 
-    void navigate() {
+    void followLine() {
         lccv::PiCamera cam;
         cam.options->video_width = 640;
         cam.options->video_height = 480;
         cam.options->framerate = 30;
-        cam.options->verbose = true;
         cam.startVideo();
-        
-        cv::Mat image1(480, 640, CV_8UC3);
-        cv::Mat image2(480, 640, CV_8UC3);
 
+        cv::Mat frame(480, 640, CV_8UC3);
         int ch = 0;
         while (ch != 27) {
-            // cam.getVideoFrame(image1, 1000);
-            // vision.getOutputVision(image1, image2);
-            // double contourX = vision.getCentroidXFirstSlices();
-            // cv::imshow("Raw", image1);
-            // cv::imshow("Processed", image2);
-            // ch = cv::waitKey(5);
-            // printf("x = %f", contourX);
-            // double vy = kp_vy * (contourX - cam_offset);
-    
-            // double omega1, omega2, omega3;
-            // kinemator.computeWheelVelocityFromRobotVelocity(vx, vy, 0.0, omega1, omega2, omega3);
-            motor.controlAngularVelocity(0.625, 0.625, 0.625, 20.0);
-            motor.stop_motor();
-            // cleanupEncoders();
+            cam.getVideoFrame(frame, 1000);
+            std::vector<cv::Point> centroids = vision.getCentroids();
+            cv::imshow("Processed Frame", frame);
+            ch = cv::waitKey(5);
+            
+            double avg_centroid_x = calculateAverageCentroidX(centroids);
+            double y_error = avg_centroid_x - cam_offset;
+            double vy = kp_vy * y_error;
+            double omega = kp_omega * y_error;
+
+            vy = std::clamp(vy, -1.0, 1.0);
+            omega = std::clamp(omega, -1.0, 1.0);
+
+            double omega1, omega2, omega3;
+            kinemator.computeWheelVelocityFromRobotVelocity(vx, vy, omega, omega1, omega2, omega3);
+            motor.controlAngularVelocity(omega1, omega2, omega3, 20.0);
         }
+        motor.stop_motor();
         cleanupEncoders();
     }
 };
 
-
-#endif // NAVIGATION_HPP 
+#endif // NAVIGATION_HPP
