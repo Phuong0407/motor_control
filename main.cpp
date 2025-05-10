c03b4a07d9e7f3928b7632cc0073146a54415230
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-10 09:50:45 +0200

    .

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 #include "./include/navigation/navigation.hpp"
 
 int main() {
-    Navigation naviation(0.1155, 0.1155, 0.026, 0.0215, 1.0, 2.0, 2.0, );
+    Navigation naviation(0.1155, 0.1155, 0.026, 0.0215, 1.0, 2.0, 2.0);
     naviation.followLine();
 }
