b8f16f1e258852adcef1f994176156c352e106c3
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-11 11:12:09 +0200

    .

--- include/motor/encoder.hpp
+++ include/motor/encoder.hpp
@@ -64,7 +64,12 @@ inline MotorEncoder* encoders[NUM_ENCODERS] = { nullptr };
 
 static void isr0() { if (encoders[0]) encoders[0]->updateCounter(); }
 static void isr1() { if (encoders[1]) encoders[1]->updateCounter(); }
-static void isr2() { if (encoders[2]) encoders[2]->updateCounter(); }
+static void isr2() {
+    if (encoders[2]) {
+        printf("[DEBUG] ISR for Motor 3 triggered. Current counter: %ld\n", encoders[2]->getCounter());        
+        encoders[2]->updateCounter();
+    }
+}
 
 typedef void (*IsrFunc)();
 static IsrFunc isr_functions[NUM_ENCODERS] = {
