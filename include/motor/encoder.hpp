c7b233cd5c1c903d3bfabfc05dec948b83a8d470
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-11 11:59:42 +0200

    modify pid

--- include/motor/encoder.hpp
+++ include/motor/encoder.hpp
@@ -8,110 +8,69 @@
 #include <stdio.h>
 #include <vector>
 
-constexpr int NUM_ENCODERS                  = 3;
-static constexpr double COUNTER_PER_REV     = 144.0;
-static constexpr double ANGLE_PER_TICK      = 2.5;
-static constexpr uint64_t DEBOUNCE_TIME_US  = 900;
+static constexpr int    NUM_ENCODERS    = 3;
+static constexpr double COUNTER_PER_REV = 144.0;
 
-class MotorEncoder {
-private:
-    int H1, H2;
-    volatile int64_t counter;
+static constexpr int MOTOR1_H1 = 21;
+static constexpr int MOTOR1_H2 = 22;
 
-    void initEncoder(int i2c_addr) {
-        if (wiringPiSetup() == -1) {
-            printf("[ERROR] wiringPiSetup failed.\n");
-            return;
-        }
+static constexpr int MOTOR2_H1 = 3;
+static constexpr int MOTOR2_H2 = 4;
 
-        int i2c_fd = wiringPiI2CSetup(i2c_addr);
-        if (i2c_fd < 0) {
-            printf("[ERROR] I2C setup failed.\n");
-        }
+static constexpr int MOTOR3_H1 = 27;
+static constexpr int MOTOR3_H2 = 0;
 
-        pinMode(H1, INPUT);
-        pinMode(H2, INPUT);
-        pullUpDnControl(H1, PUD_UP);
-        pullUpDnControl(H2, PUD_UP);
+volatile int64_t counter1 = 0;
+volatile int64_t counter2 = 0;
+volatile int64_t counter3 = 0;
 
-        if (wiringPiI2CRead(i2c_fd) == -1) {
-            printf("[ERROR] I2C device is not responding.\n");
-            return;
-        }
-        printf("[INFO] Encoder initialized on pins H1 = %d, H2 = %d.\n", H1, H2);
+void initEncoder(int i2c_addr, int H1, int H2) {
+    if (wiringPiSetup() == -1) {
+        printf("[ERROR] wiringPiSetup failed.\n");
+        return;
     }
+    int i2c_fd = wiringPiI2CSetup(i2c_addr);
+    if (i2c_fd < 0) printf("[ERROR] I2C setup failed.\n");
 
-public:
-    MotorEncoder(int H1, int H2, int i2c_addr = 0x0f) :
-    H1(H1), H2(H2), counter(0)
-    {
-        initEncoder(i2c_addr);
+    pinMode(H1, INPUT); pullUpDnControl(H1, PUD_UP);
+    pinMode(H2, INPUT); pullUpDnControl(H2, PUD_UP);
+
+    if (wiringPiI2CRead(i2c_fd) == -1) {
+        printf("[ERROR] I2C device is not responding.\n");
+        return;
     }
-
-
-    void updateCounter() {
-        digitalRead(H2) ? ++counter : --counter;
-    }
-
-    void resetCounter() {
-        counter = 0;
-    }
-
-    int64_t getCounter() const { return counter; }
-};
-
-inline MotorEncoder* encoders[NUM_ENCODERS] = { nullptr };
-
-static void isr0() { if (encoders[0]) encoders[0]->updateCounter(); }
-static void isr1() { if (encoders[1])
-    {
-        printf("[DEBUG] ISR for Motor 2 triggered. Current counter: %ld\n", encoders[1]->getCounter());
-        encoders[1]->updateCounter(); 
-    }
-}
-static void isr2() {
-    if (encoders[2]) {
-        printf("[DEBUG] ISR for Motor 3 triggered. Current counter: %ld\n", encoders[2]->getCounter());        
-        encoders[2]->updateCounter();
-    }
+    printf("[INFO] Encoder initialized on pins H1 = %d, H2 = %d.\n", H1, H2);
 }
 
-typedef void (*IsrFunc)();
-static IsrFunc isr_functions[NUM_ENCODERS] = {
-    isr0, isr1, isr2
-};
+void updateCounter1() { digitalRead(MOTOR1_H2) ? ++counter1 : --counter1; }
+void updateCounter2() { digitalRead(MOTOR2_H2) ? ++counter2 : --counter2; }
+void updateCounter3() { digitalRead(MOTOR3_H2) ? ++counter3 : --counter3; }
 
-static const int encoder_pin_table[NUM_ENCODERS][2] = {
-    {21, 22},
-    {3 , 4 },
-    {27, 0 }
-};
-
-inline void declareEncoders(int driver1_addr, int driver2_addr) {
-    for (int i = 0; i < NUM_ENCODERS; ++i) {
-        int H1 = encoder_pin_table[i][0];
-        int H2 = encoder_pin_table[i][1];
-        int addr = (i == NUM_ENCODERS - 1) ? driver2_addr : driver1_addr;
-        encoders[i] = new MotorEncoder(H1, H2, addr);
-    }
+void getTicks(int64_t &ticks1, int64_t &ticks2, int64_t &ticks3) {
+    ticks1 = counter1; ticks2 = counter2; ticks3 = counter3;
 }
 
-inline void cleanupEncoders() {
-    for (int i = 0; i < NUM_ENCODERS; ++i) {
-        delete encoders[i];
-        encoders[i] = nullptr;
-    }
+void printAttachementInfo(int AttchmState, int H1) {
+    if (AttchmState < 0) {
+        printf("[ERROR] Failed to attach ISR to pin %d.\n", H1);
+        return;
+    } else printf("[INFO] ISR attached to pin %d\n", H1);
 }
 
-inline void attachEncoderInterrupts() {
-    for (std::size_t i = 0; i < NUM_ENCODERS; ++i) {
-        int H1 = encoder_pin_table[i][0];
-        if (wiringPiISR(H1, INT_EDGE_RISING, isr_functions[i]) < 0) {
-            printf("[ERROR] Failed to attach ISR to pin %d.\n", H1);
-            return;
-        }
-        printf("[INFO] ISR attached to pin %d\n", H1);
-    }
+void attachEncoderInterrupts() {
+    int AttchmState1 = wiringPiISR(MOTOR1_H1, INT_EDGE_RISING, updateCounter1);
+    int AttchmState2 = wiringPiISR(MOTOR2_H1, INT_EDGE_RISING, updateCounter2);
+    int AttchmState3 = wiringPiISR(MOTOR3_H1, INT_EDGE_RISING, updateCounter3);
+    printAttachementInfo(AttchmState1, MOTOR1_H1);
+    printAttachementInfo(AttchmState2, MOTOR2_H1);
+    printAttachementInfo(AttchmState3, MOTOR3_H1);
+}
+
+void startEncoders() {
+    initEncoder(0x0f, MOTOR1_H1, MOTOR1_H2);
+    initEncoder(0x0f, MOTOR2_H1, MOTOR2_H2);
+    initEncoder(0x0d, MOTOR3_H1, MOTOR3_H2);
+    attachEncoderInterrupts();
 }
 
 #endif // ENCODER_HPP
---
+++ include/motor/pid.hpp
@@ -0,0 +1,49 @@
+#ifndef PID_HPP
+#define PID_HPP
+
+#include <cmath>
+#include <algorithm>
+
+class PID {
+private:
+    double kp, ki, kd, Alpha, smpl_intv;
+    double intgr, prev_fltrerr;
+    double max_out, max_intgr = 200.0;
+
+    static double calcAlphaEMA(double cutoff_freq) {
+        if (cutoff_freq <= 0.0)
+            return 1.0;
+        const double Alpha_cutoff = std::cos(2 * M_PI * cutoff_freq);
+        return Alpha_cutoff - 1 + std::sqrt(Alpha_cutoff * Alpha_cutoff - 4 * Alpha_cutoff + 3);
+    }
+
+public:
+PID(
+        double kp,
+        double ki,
+        double kd,
+        double max_out,
+        double cutoff_freq,
+        double smpl_intv
+    ) :
+    kp(kp), ki(ki), kd(kd),
+    Alpha(calcAlphaEMA(cutoff_freq * smpl_intv)),
+    smpl_intv(smpl_intv),
+    intgr(0.0), prev_fltrerr(0.0),
+    max_out(max_out)
+    {}
+
+    double compute(double ref, double measured) {
+        double err = ref - measured;
+        double fltrerr = Alpha * err + (1.0 - Alpha) * prev_fltrerr;
+        double dev = (fltrerr - prev_fltrerr) / smpl_intv;
+        double new_intgr = intgr + err * smpl_intv;
+        intgr = std::clamp(new_intgr, -max_intgr, max_intgr);
+        double ctrl_sgnl = kp * err + ki * intgr + kd * dev;
+        ctrl_sgnl = std::clamp(ctrl_sgnl, -max_out, max_out);
+        prev_fltrerr = fltrerr;
+        return ctrl_sgnl;
+    }
+};
+
+#endif // PID_HPP
