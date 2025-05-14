9ea357b2230812140d3846b95097c2673d397506
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-14 16:19:47 +0200

    modify camera part in navigation

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[]) {
     double kp_omega = std::stod(argv[1]);
 
-    Navigation naviation(0.1155, 0.245, 0.0273, 640, 240, 30, false, 1.0, 0.0001);
+    Navigation naviation(0.1155, 0.245, 0.0273, 640, 480, 30, false, 1.0, 0.0001);
     naviation.followLine();
     return 0;
 }
