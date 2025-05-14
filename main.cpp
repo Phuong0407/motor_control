61f0da9cbd37603444a3243b9697f9fb398e59fd
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-14 15:37:42 +0200

    modify main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
-    double kp_omega = stod(argv[1]);
+    double kp_omega = std::stod(argv[1]);
 
     Navigation naviation(0.1155, 0.245, 0.0273, 640, 240, 30, false, 1.0, 0.0001);
     naviation.followLine();
