439f7c1552582a3f1c2454243f0fcc408fb3ef61
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-15 17:44:33 +0200

    .

--- test/test_motor.cpp
+++ test/test_motor.cpp
@@ -5,7 +5,7 @@
 int main() {
     Motor motor;
     motor.setPIDParameters(6.0, 0.5, 0.01, 1.0);
-    bool control_state = motor.controlAngularVelocity(0.625, 0.625, 0.625);
+    bool control_state = motor.controlAngularVelocity(0.625, 0.625, 0.0);
     if (control_state) {
         printf("\nMotor control successful.\n");
     } else {
