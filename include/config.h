3f04090d3df2855d547f07eab95e6cdf921f25a2
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-16 10:31:25 +0200

    add global config

---
+++ include/config.h
@@ -0,0 +1,18 @@
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+constexpr int encoder_addr1 = 0x0f;
+constexpr int encoder_addr2 = 0x0d;
+
+int i2c_fd1 = -1;
+int i2c_fd2 = -1;
+
+static constexpr int MOTOR1_H1 = 21;
+static constexpr int MOTOR1_H2 = 22;
+static constexpr int MOTOR2_H1 = 3;
+static constexpr int MOTOR2_H2 = 4;
+static constexpr int MOTOR3_H1 = 27;
+static constexpr int MOTOR3_H2 = 0;
+
+static constexpr double COUNTER_PER_REV = 144.0;
+static constexpr double smpl_itv = 0.1;
