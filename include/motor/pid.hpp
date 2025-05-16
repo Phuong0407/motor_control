39cad9070583e00988997fa1da2424fdeb1d6b61
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-16 09:46:57 +0200

    .

--- include/motor/pid.hpp
+++ include/motor/pid.hpp
@@ -1,49 +1,41 @@
 #ifndef PID_HPP
 #define PID_HPP
 
-#include <cmath>
 #include <algorithm>
 
 class PID {
 private:
-    double kp, ki, kd, Alpha, smpl_intv;
-    double intgr, prev_fltrerr;
-    double max_out, max_intgr = 80.0;
-
-    static double calcAlphaEMA(double cutoff_freq) {
-        if (cutoff_freq <= 0.0)
-            return 1.0;
-        const double Alpha_cutoff = std::cos(2 * M_PI * cutoff_freq);
-        return Alpha_cutoff - 1 + std::sqrt(Alpha_cutoff * Alpha_cutoff - 4 * Alpha_cutoff + 3);
-    }
+    double kp, ki, kd;
+    double smpl_intv;
+    double intgr, prev_err;
+    double max_out, max_intgr;
 
 public:
-PID() = default;
+    PID() = default;
 
-PID(
+    PID(
         double kp,
         double ki,
         double kd,
         double max_out,
-        double cutoff_freq,
+        double max_intgr,
         double smpl_intv
     ) :
-    kp(kp), ki(ki), kd(kd),
-    Alpha(calcAlphaEMA(cutoff_freq * smpl_intv)),
-    smpl_intv(smpl_intv),
-    intgr(0.0), prev_fltrerr(0.0),
-    max_out(max_out)
+        kp(kp), ki(ki), kd(kd),
+        smpl_intv(smpl_intv),
+        max_out(max_out),
+        max_intgr(max_intgr),
+        intgr(0.0), prev_err(0.0)
     {}
 
     double compute(double ref, double measured) {
         double err = ref - measured;
-        double fltrerr = Alpha * err + (1.0 - Alpha) * prev_fltrerr;
-        double dev = (fltrerr - prev_fltrerr) / smpl_intv;
-        double new_intgr = intgr + err * smpl_intv;
-        intgr = std::clamp(new_intgr, -max_intgr, max_intgr);
+        intgr += err * smpl_intv;
+        intgr = std::clamp(intgr, -max_intgr, max_intgr);
+        double dev = (err - prev_err) / smpl_intv;
         double ctrl_sgnl = kp * err + ki * intgr + kd * dev;
         ctrl_sgnl = std::clamp(ctrl_sgnl, -max_out, max_out);
-        prev_fltrerr = fltrerr;
+        prev_err = err;
         return ctrl_sgnl;
     }
 };
