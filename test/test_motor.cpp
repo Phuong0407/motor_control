3d355ba8b31736d8ba30f07c81d81a760f40420d
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-16 08:14:49 +0200

    .

--- test/test_motor.cpp
+++ test/test_motor.cpp
@@ -3,27 +3,27 @@
 #include <iostream>
 
 int main() {
-    Motor motor;
-    motor.setPIDParameters(6.0, 0.5, 0.01, 1.0);
-    bool control_state = motor.controlAngularVelocity(0.625, 0.625, 0.625);
-    if (control_state) {
-        printf("\nMotor control successful.\n");
-    } else {
-        printf("\nMotor control failure.\n");
-    }
+    // Motor motor;
+    // motor.setPIDParameters(6.0, 0.5, 0.01, 1.0);
+    // bool control_state = motor.controlAngularVelocity(0.625, 0.625, 0.625);
+    // if (control_state) {
+    //     printf("\nMotor control successful.\n");
+    // } else {
+    //     printf("\nMotor control failure.\n");
+    // }
 
-    wiringPiI2CWriteReg16(i2c_fd2, 0x82, (255));
-    delay(1);
-    wiringPiI2CWriteReg16(i2c_fd2, 0xaa, 0x06);
+    // wiringPiI2CWriteReg16(i2c_fd2, 0x82, (255));
+    // delay(1);
+    // wiringPiI2CWriteReg16(i2c_fd2, 0xaa, 0x06);
 
-    motor.~Motor();
-    // startEncoders();
-    // std:: cout << i2c_fd1 << std::endl;
-    // std:: cout << i2c_fd2 << std::endl;
+    // motor.~Motor();
+    startEncoders();
+    std:: cout << i2c_fd1 << std::endl;
+    std:: cout << i2c_fd2 << std::endl;
 
-    // setMotorPWM(255, 255, 255);
-    // delay(1000);
-    // setMotorPWM(0, 0, 0);
+    setMotorPWM(255, 255, 255);
+    delay(1000);
+    setMotorPWM(0, 0, 0);
 
     return 0;
 }
