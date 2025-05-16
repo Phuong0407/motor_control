40107e4e46a5fdf8f780df76d11c70eca2c84022
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-16 14:20:33 +0200

    .

--- include/motor/motor_control.hpp
+++ include/motor/motor_control.hpp
@@ -100,7 +100,7 @@ public:
             double err_thres = std::max(ERROR_THRESHOLD_PERCENT * std::abs(ref1), MIN_ERROR_RPS);
             int pwm1, pwm2, dir1, dir2;
             if (std::abs(err1) > err_thres + 1e-6) {
-                computed1 = pid1.compute(err1/max_rps);
+                computed1 = pid1.compute(err1);
                 printf("Motor 1: %.3f\t%.3f\t%.3f\n", measured1, computed1, err1 * 100.0);
                 dir1 = (computed1 > 0) ? FORWARD : BACKWARD;
                 pwm1 = computePWMFromRPS(std::abs(computed1));
