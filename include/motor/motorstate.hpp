f7792bc03a7f61a7d99f609bd9b2ea42a2eb6507
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-16 23:11:14 +0200

    modify motorstate

--- include/motor/motorstate.hpp
+++ include/motor/motorstate.hpp
@@ -3,30 +3,53 @@
 
 #include <cmath>
 #include <algorithm>
+#include <iostream>
 
 static constexpr int MAX_PWM                = 255;
-static constexpr double SAFTY_OFFSET        = 0.8;
+static constexpr double SAFETY_OFFSET       = 0.7;
 static constexpr double SCALEUP_FACTOR_PWM  = 0.72549019607;
 static constexpr int BACKWARD               = -1;
-static constexpr int FORWARD                = +1;
-static constexpr int LEFT                   = -1;
-static constexpr int RIGHT                  = +1;
+static constexpr int FORWARD                = 1;
 static constexpr int STOP                   = 0;
-static constexpr double max_rps             = 0.86;
+static constexpr double MAX_RPS             = 0.86;
 static constexpr int DEAD_PWM               = 70;
 
-int computePWMFromRPS(double u_rps) {
-    double norm_rps = std::clamp(u_rps / max_rps, 0.0, 1.0);
-    int pwm = MAX_PWM * static_cast<int>(std::round(norm_rps * SAFTY_OFFSET));
-    if (pwm < DEAD_PWM) pwm = 0;
-    else pwm = static_cast<int>(pwm - DEAD_PWM) / SCALEUP_FACTOR_PWM;
-    return pwm;
-}
-
-class MotorState{
-public:
+class MotorState {
+private:
     int pwm = 0;
-    int dir  = FORWARD;
+    int dir = FORWARD;
+
+    static int computePWMFromRPS(double u_rps) {
+        double norm_rps = std::clamp(u_rps / MAX_RPS, 0.0, 1.0);
+        int pwm_value = static_cast<int>(MAX_PWM * norm_rps * SAFETY_OFFSET);
+
+        if (pwm_value < DEAD_PWM) return 0;
+        return static_cast<int>((pwm_value - DEAD_PWM) / SCALEUP_FACTOR_PWM);
+    }
+    
+public:
+    MotorState() = default;
+    MotorState(int pwm, int dir) { setMotorState(pwm, dir);}
+    
+    void setMotorState(int pwm, int dir) {
+        this->pwm = std::clamp(pwm, 0, MAX_PWM);
+        this->dir = dir;        
+    }
+
+    void setMotorStateRPS(double rps) {
+        if(rps == 0.0) {
+            setMotorState(0, STOP);
+            return;
+        }
+        dir = (rps > 0) ? FORWARD : BACKWARD;
+        pwm = computePWMFromRPS(rps);
+    }
+
+    void stop() { pwm = 0; }
+    void reverse() { dir = (dir == FORWARD) ? BACKWARD : FORWARD; }
+
+    inline int getPWM() const { return pwm; }
+    inline int getDirection() const { return dir; }
 };
 
 #endif // MOTORSTATE_HPP
