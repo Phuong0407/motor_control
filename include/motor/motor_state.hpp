7f1d333213c98b6e1c01ec9cbc8624a00d9da701
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-16 10:24:34 +0200

    handle dead zone pwm

--- include/motor/motor_state.hpp
+++ include/motor/motor_state.hpp
@@ -1,7 +1,16 @@
 #ifndef MOTOR_STATE_HPP
 #define MOTOR_STATE_HPP
 
-#define MAX_PWM 255
+#include <cmath>
+#include <algorithm>
+
+static constexpr int MAX_PWM                = 255;
+static constexpr double SAFTY_OFFSET        = 0.8;
+static constexpr double SCALEUP_FACTOR_PWM  = 0.72549019607;
+static constexpr int BACKWARD               = -1;
+static constexpr int FORWARD                = +1;
+static constexpr int LEFT                   = -1;
+static constexpr int RIGHT                  = +1;
 
 class MotorState{
 public:
@@ -10,8 +19,16 @@ public:
     double ref;
     double measure;
     double compute;
-    double max_rps;
-    double min_rps;
+    const double max_rps = 0.86;
+    const int DEAD_PWM = 70;
+
+    int computePWMFromRPS(double u_rps) {
+        double norm_rps = std::clamp(u_rps / max_rps, 0.0, 1.0);
+        int pwm = MAX_PWM * static_cast<int>(std::round(norm_rps * SAFTY_OFFSET));
+        if (pwm < DEAD_PWM) pwm = 0;
+        else pwm = static_cast<int>(pwm - DEAD_PWM) / SCALEUP_FACTOR_PWM;
+        return pwm;
+    }
 };
 
 #endif // MOTOR_STATE_HPP
