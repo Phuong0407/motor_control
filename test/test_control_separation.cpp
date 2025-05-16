7b40e0c7c298e23efb80cfb768ee7b0b3d00b695
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-16 13:47:33 +0200

    .

--- test/test_control_separation.cpp
+++ test/test_control_separation.cpp
@@ -1,7 +1,7 @@
 #include "motor_control.hpp"
 
 int main() {
-    MotorControl controller(6.0, 0.5, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    MotorControl controller(2.0, 0.5, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
     controller.controlMotor1(0.625);
     controller.stopMotors();
     return 0;
