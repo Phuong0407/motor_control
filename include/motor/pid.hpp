3b5d659314b4211b8fc1bdc9d1f4d7be4aab8de2
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-16 23:12:19 +0200

    add before resolve conflict

--- include/motor/pid.hpp
+++ include/motor/pid.hpp
@@ -1,49 +1,55 @@
 #ifndef PID_HPP
 #define PID_HPP
 
+#include "config.h"
+
 #include <cmath>
 #include <algorithm>
 
 class PID {
+private:
+    double kp = 0.0;
+    double ki = 0.0;
+    double kd = 0.0;
+    double intgr = 0.0;
+    double prev_err = 0.0;
+    double ema_coef = 0.0;
+    double max_out = 0.0;
+
 public:
     PID() = default;
 
-    PID(
-        double kp,
-        double ki,
-        double kd,
-        double cutoff_freq,
-        double max_out
-    ) :
-    kp(kp),
-    ki(ki),
-    kd(kd),
-    max_out(max_out)
-    { 
+    PID(double kp, double ki, double kd, double cutoff_freq, double max_out) 
+        : kp(kp), ki(ki), kd(kd), max_out(max_out)
+    {
+        this->ki = ki;
+        this->kd = kd;
+        setEMACutoff(cutoff_freq);
+    }
+
+
+    void setPIDParameters(double kp, double ki, double kd, double cutoff_freq, double max_out) {
+        this->kp = kp;
+        this->ki = ki;
+        this->kd = kd;
+        this->max_out = max_out;
         setEMACutoff(cutoff_freq);
     }
 
     double compute(double input) {
-        double error = ref - input;
-        intgr += error * smpl_itv;
-        intgr = std::clamp(intgr, -max_out, max_out);
+        double err = ref - input;
+        double new_intgr = intgr + err * smpl_itv;
         double dev = EMAFltCoeff * (prev_input - input);
-        prev_input -= dev;
-        double output = kp * error + ki * intgr + kd * dev;
-        output = std::clamp(output, -max_out, max_out);
         prev_input = input;
+
+        double output = kp * err + ki * new_intgr + kd * dev;
+
+        output = std::clamp(output, -max_out, max_out);
+        if (std::abs(output) != max_out) intgr = new_intgr;
         return output;
     }
 
-    void setKp(double kp) { this->kp = kp; }
-    void setKi(double ki) { this->ki = ki * smpl_itv; }
-    void setKd(double kd) { this->kd = kd / smpl_itv; }
-
-    double getKp() const { return kp; }
-    double getKi() const { return ki / smpl_itv; }
-    double getKd() const { return kd * smpl_itv; }
-
-    void setEMACutoff(double cutoff_freq) {
+    inline void setEMACutoff(double cutoff_freq) {
         if (cutoff_freq <= 0.0) {
             EMAFltCoeff = 1.0;
         } else {
@@ -52,28 +58,8 @@ public:
         }
     }
 
-    void setSetpoint(double ref) { this->ref = ref;
-    }
-
-    double getSetpoint() const { return ref; }
-
-    void resetIntegral() { intgr = 0.0; }
-
-    void setMaxOutput(double max_out) {
-        this->max_out = max_out;
-    }
-
-    double getMaxOutput() const { return max_out; }
-
-private:
-    double kp = 1.0;
-    double ki = 0.0;
-    double kd = 0.0;
-    double max_out = 0.0;
-    double EMAFltCoeff = 1.0;
-    double prev_input = 0.0;
-    double intgr = 0.0;
-    double ref = 0.0;
+    inline void setRef(double ref) { this->ref = ref; }
+    inline void resetIntegral() { intgr = 0.0; }
 };
 
 #endif // PID_HPP
