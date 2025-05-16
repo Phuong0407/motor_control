728b01f5355849d7bd27cae792979e9c39b0edde
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-16 11:27:20 +0200

    .

--- test/test_motor_separate.cpp
+++ test/test_motor_separate.cpp
@@ -3,93 +3,89 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// Pin Definitions
-#define IN1_PIN  5   // Motor A Direction 1
-#define IN2_PIN  6   // Motor A Direction 2
-#define ENA_PIN  12  // Motor A PWM
+// Motor A (Driver 1)
+#define IN1_PIN  5
+#define IN2_PIN  6
+#define ENA_PIN  12  // PWM0
 
-#define IN3_PIN  13  // Motor B Direction 1
-#define IN4_PIN  19  // Motor B Direction 2
-#define ENB_PIN  26  // Motor B PWM
+// Motor B (Driver 1)
+#define IN3_PIN  13
+#define IN4_PIN  19
+#define ENB_PIN  26  // PWM1
+
+// Motor C (Driver 2)
+#define IN5_PIN  16
+#define IN6_PIN  20
+#define ENC_PIN  21  // PWM1
 
 void setupPins() {
     wiringPiSetupGpio();  // Use BCM GPIO numbering
 
+    // Motor A
     pinMode(IN1_PIN, OUTPUT);
     pinMode(IN2_PIN, OUTPUT);
     pinMode(ENA_PIN, PWM_OUTPUT);
 
+    // Motor B
     pinMode(IN3_PIN, OUTPUT);
     pinMode(IN4_PIN, OUTPUT);
     pinMode(ENB_PIN, PWM_OUTPUT);
 
-    // Set PWM range to 1024 (default is 1024)
-    pwmSetRange(1024);
+    // Motor C
+    pinMode(IN5_PIN, OUTPUT);
+    pinMode(IN6_PIN, OUTPUT);
+    pinMode(ENC_PIN, PWM_OUTPUT);
+
+    // Set PWM frequency and range
     pwmSetMode(PWM_MODE_MS);
-    pwmSetClock(384);  // Adjust to change PWM frequency (1.2 kHz approx)
+    pwmSetRange(1024);
+    pwmSetClock(384);  // ~1.2 kHz PWM frequency
 }
 
-void controlMotorA(int speed, int direction) {
+void controlMotor(int in1, int in2, int pwmPin, int speed, int direction) {
     // Clamp speed to 0 - 1024
     speed = (speed > 1024) ? 1024 : (speed < 0) ? 0 : speed;
 
-    // Set Direction
+    // Set direction
     if (direction == 1) {
-        digitalWrite(IN1_PIN, HIGH);
-        digitalWrite(IN2_PIN, LOW);
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
     } else if (direction == -1) {
-        digitalWrite(IN1_PIN, LOW);
-        digitalWrite(IN2_PIN, HIGH);
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
     } else {
-        digitalWrite(IN1_PIN, LOW);
-        digitalWrite(IN2_PIN, LOW);
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, LOW);
     }
 
     // Set PWM speed
-    pwmWrite(ENA_PIN, speed);
-}
-
-void controlMotorB(int speed, int direction) {
-    // Clamp speed to 0 - 1024
-    speed = (speed > 1024) ? 1024 : (speed < 0) ? 0 : speed;
-
-    // Set Direction
-    if (direction == 1) {
-        digitalWrite(IN3_PIN, HIGH);
-        digitalWrite(IN4_PIN, LOW);
-    } else if (direction == -1) {
-        digitalWrite(IN3_PIN, LOW);
-        digitalWrite(IN4_PIN, HIGH);
-    } else {
-        digitalWrite(IN3_PIN, LOW);
-        digitalWrite(IN4_PIN, LOW);
-    }
-
-    // Set PWM speed
-    pwmWrite(ENB_PIN, speed);
+    pwmWrite(pwmPin, speed);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
-        printf("Usage: %s <motorA_speed> <motorA_dir> <motorB_speed> <motorB_dir>\n", argv[0]);
+    if (argc != 7) {
+        printf("Usage: %s <MotorA_speed> <MotorA_dir> <MotorB_speed> <MotorB_dir> <MotorC_speed> <MotorC_dir>\n", argv[0]);
         printf("Direction: 1 = Forward, -1 = Backward, 0 = Stop\n");
         return 1;
     }
 
-    // Parse command line arguments
     int motorA_speed = atoi(argv[1]);
     int motorA_dir = atoi(argv[2]);
     int motorB_speed = atoi(argv[3]);
     int motorB_dir = atoi(argv[4]);
+    int motorC_speed = atoi(argv[5]);
+    int motorC_dir = atoi(argv[6]);
 
     setupPins();
 
     // Control Motors
-    controlMotorA(motorA_speed, motorA_dir);
-    controlMotorB(motorB_speed, motorB_dir);
+    controlMotor(IN1_PIN, IN2_PIN, ENA_PIN, motorA_speed, motorA_dir);
+    controlMotor(IN3_PIN, IN4_PIN, ENB_PIN, motorB_speed, motorB_dir);
+    controlMotor(IN5_PIN, IN6_PIN, ENC_PIN, motorC_speed, motorC_dir);
 
     printf("Motor A: Speed %d, Direction %d\n", motorA_speed, motorA_dir);
     printf("Motor B: Speed %d, Direction %d\n", motorB_speed, motorB_dir);
+    printf("Motor C: Speed %d, Direction %d\n", motorC_speed, motorC_dir);
 
     printf("Press CTRL+C to exit...\n");
     while (1) {
