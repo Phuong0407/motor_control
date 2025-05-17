4146c060cb377f19d3a4763fd6eed7ffc23e4f80
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-17 21:42:18 +0200

    change rps_err to tick_err

--- include/motor/motor.hpp
+++ include/motor/motor.hpp
@@ -59,7 +59,7 @@ static constexpr double MAX_TICKS                 = 9.0;
 static constexpr double SAFETY_OFFSET           = 0.8;
 static constexpr double DEADZONE_SCALEUP        = 0.843137254901961;
 static constexpr double ERROR_THRESHOLD_PERCENT = 0.10;
-static constexpr double MIN_ERROR_RPS           = 0.10;
+static constexpr double MIN_ERROR_TICKS         = 1.0;
 
 
 
