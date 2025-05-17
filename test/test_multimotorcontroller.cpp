82054c2343a52ca289617e1d71f3d7189bf566cb
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-17 05:57:11 +0200

    run just motor1 thread

--- test/test_multimotorcontroller.cpp
+++ test/test_multimotorcontroller.cpp
@@ -18,13 +18,13 @@ int main() {
     motorController.setMotor3Reference(0.625);
 
     std::thread motor1Thread(&MotorController::controlMotor1, &motorController);
-    std::thread motor2Thread(&MotorController::controlMotor2, &motorController);
-    std::thread motor3Thread(&MotorController::controlMotor3, &motorController);
+    // std::thread motor2Thread(&MotorController::controlMotor2, &motorController);
+    // std::thread motor3Thread(&MotorController::controlMotor3, &motorController);
     std::thread monitorThread(&MotorController::monitorMotorsSpeed, &motorController);
 
     motor1Thread.join();
-    motor2Thread.join();
-    motor3Thread.join();
+    // motor2Thread.join();
+    // motor3Thread.join();
     monitorThread.join();
 
     return 0;
