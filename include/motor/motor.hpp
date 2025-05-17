0076adf5a88c8fe6d2a90ee94cfb4e4762136fa5
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-17 22:31:47 +0200

    .

--- include/motor/motor.hpp
+++ include/motor/motor.hpp
@@ -180,6 +180,7 @@ void controlMotor1(void *arg) {
         double LOWER = ref1 - MIN_ERROR_TPS;
         double UPPER = ref1 + MIN_ERROR_TPS;
         double err1 = ref1 - measured1;
+
         if (measured1 >= LOWER && measured1 <= UPPER) err1 = 0.0;
 
         if (std::abs(err1) > 1e-6) {
@@ -197,8 +198,13 @@ void controlMotor2(void *arg) {
         curr_ticks2 = counter2;
         measured2 = static_cast<double>(curr_ticks2 - prev_ticks2) / 0.1;
 
+        double LOWER = ref1 - MIN_ERROR_TPS;
+        double UPPER = ref1 + MIN_ERROR_TPS;
         double err2 = ref2 - measured2;
-        if (std::abs(err2) > MIN_ERROR_TPS + 1e-6) {
+
+        if (measured1 >= LOWER && measured1 <= UPPER) err2 = 0.0;
+
+        if (std::abs(err2) > 1e-6) {
             computed2 = pid2.compute(ref2, measured2);
             setMotor2(computed2);
         }
@@ -213,8 +219,15 @@ void controlMotor3(void *arg) {
         curr_ticks3 = counter3;
         measured3 = static_cast<double>(prev_ticks3 - curr_ticks3) / 0.1;
 
+        double LOWER = ref1 - MIN_ERROR_TPS;
+        double UPPER = ref1 + MIN_ERROR_TPS;
+        
         double err3 = ref3 - measured3;
-        if (std::abs(err3) > MIN_ERROR_TPS + 1e-6) {
+
+        if (measured1 >= LOWER && measured1 <= UPPER) err3 = 0.0;
+
+
+        if (std::abs(err3) > 1e-6) {
             computed3 = pid3.compute(ref3, measured3);
             setMotor3(computed3);
         }
