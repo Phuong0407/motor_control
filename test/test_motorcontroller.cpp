e039dc87d5878f01c1fbd1257e3de75563452ca4
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-17 03:32:10 +0200

    fine tune pid motor 2

--- test/test_motorcontroller.cpp
+++ test/test_motorcontroller.cpp
@@ -9,7 +9,7 @@ int main() {
         6.0, 0.5, 0.05, 4.0, 0.86
     );
     motorController.setMotor1Reference(0.5);
-    motorController.setMotor2Reference(0.5);
+    motorController.setMotor2Reference(-0.5);
     motorController.setMotor3Reference(0.625);
 
     // motorController.controlMotor1();
