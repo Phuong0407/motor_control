7f0cd5cbab0c1d13213ebb04d79717602032bcca
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-18 01:51:34 +0200

    modify robot

--- include/robot.h
+++ include/robot.h
@@ -1,5 +1,5 @@
-#ifndef ROBOT_HPP
-#define ROBOT_HPP
+#ifndef ROBOT_H
+#define ROBOT_H
 
 #include "pid.h"
 
@@ -20,10 +20,35 @@ double measured2        = 0.0;
 double measured3        = 0.0;
 double computed1        = 0.0;
 double computed2        = 0.0;
-double computed3        = 0.0;
+double computed3        
 PID                     pid1;
 PID                     pid2;
-PID                     pid3;
+PID                     
 lccv::PiCamera          cam;
 
-#endif // ROBOT_HPP
+static constexpr int    ADDRESS1                = 0x0f;
+static constexpr int    ADDRESS2                = 0x0d;
+static constexpr int    MOTOR1_H1               = 21;
+static constexpr int    MOTOR1_H2               = 22;
+static constexpr int    MOTOR2_H1               = 3;
+static constexpr int    MOTOR2_H2               = 4;
+static constexpr int    MOTOR3_H1               = 27;
+static constexpr int    MOTOR3_H2               = 0;
+static constexpr int    MAX_PWM                 = 255;
+static constexpr int    DEAD_PWM                = 40;
+
+static constexpr double SAFETY_OFFSET           = 0.8;
+static constexpr double DEADZONE_SCALEUP        = 0.843137254901961;
+static constexpr double ERROR_THRESHOLD_PERCENT = 0.10;
+static constexpr double MIN_ERROR_TPS           = 11.0;
+
+#ifdef NOLOAD_RUN
+static constexpr double MAX_TPS                 = 12.0;
+#else // LOAD_RUN
+static constexpr double MAX_TPS                 = 9.0;
+#endif
+
+static           int    i2c_fd1                 = -1;
+static           int    i2c_fd2                 = -1;
+
+#endif // ROBOT_H
