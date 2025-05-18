0dc1aca23db3ab4f303b684c785df5d4d904bf02
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-18 02:54:06 +0200

    fix bug

--- include/vision.h
+++ include/vision.h
@@ -16,7 +16,7 @@ void * computeBaryCenter(void *arg) {
             continue;
         }
 
-        cv::cvtColor(img, img_hsv, COLOR_BGR2HSV);
+        cv::cvtColor(img, img_hsv, cv::COLOR_BGR2HSV);
         cv::inRange(img_hsv, cv::Scalar(0, 120, 70),    cv::Scalar(10, 255, 255),   red1);
         cv::inRange(img_hsv, cv::Scalar(170, 120, 70),  cv::Scalar(180, 255, 255),  red2);
         cv::inRange(img_hsv, cv::Scalar(100,150,50),    cv::Scalar(140, 255, 255),  blue);
@@ -24,17 +24,17 @@ void * computeBaryCenter(void *arg) {
 
         std::vector<cv::Vec4i>              hierarchy;
         std::vector<std::vector<cv::Point>> contours;
-        findContours(bin_mask, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
+        findContours(bin_mask, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
 
         cv::Mat img_cpy1 = img.clone();
-        cv::drawContours(img_cpy1, contours, -1, Scalar(0, 255, 0), 2);
+        cv::drawContours(img_cpy1, contours, -1, cv::Scalar(0, 255, 0), 2);
         
         cv::Mat img_cpy2    = img.clone();
         cv::Moments moment  = cv::moments(bin_mask, true);
         x                   = moment.m10 / moment.m00;
 
-        cv::Point p(barycenter, moment.m01 / moment.m00);
-        cv::circle(image_cpy2, p, 5, Scalar(128,0,0), -1);
+        cv::Point p(x, moment.m01 / moment.m00);
+        cv::circle(img_cpy2, p, 5, Scalar(128,0,0), -1);
         cv::imshow("CAMERA", img_cpy2);
 
         printf("x = %d\n", x);
@@ -42,6 +42,7 @@ void * computeBaryCenter(void *arg) {
         char key = static_cast<char>(cv::waitKey(5));
         if (key == 27) break;
     }
+    return 0;
 }
 
 #endif // VISION_H
