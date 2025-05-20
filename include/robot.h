006f24311ccb213c50007e153f77f6072218e783
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-20 22:11:05 +0200

    fine tunning P control

--- include/robot.h
+++ include/robot.h
@@ -119,7 +119,7 @@ double                  extents[N_SLICES]           = {0.0};
 constexpr int           TURN_THRESHOLD              = 10;
 constexpr double        TURN_SPEED_DECREASE         = 0.5;
 constexpr double        TURN_SPEED_INCREASE         = 0.5;
-constexpr double        kp_dir                      = 0.01;
+constexpr double        kp_dir                      = 0.005;
 constexpr double        kd_dir                      = 0.01;
 
 int                     dir_offset_diffs[N_SLICES]  = {0};
