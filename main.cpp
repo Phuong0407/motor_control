6a0d7de3588eb52b867570a519a22c295d24d4d1
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-20 21:23:59 +0200

    test motor alone

--- main.cpp
+++ main.cpp
@@ -13,29 +13,30 @@
 #include <pthread.h>
 #include <stdio.h>
 
+ref1 = 100.0, ref2 = 90.0, ref3 = 95.0;
+
 int main(int argc, char *argv[]) {
 
-	cam.options->video_width    = framewidth;
-	cam.options->video_height   = frameheight;
-	cam.options->framerate      = framerate;
-	cam.options->verbose        = verbose;
-	cam.startVideo();
+	// cam.options->video_width    = framewidth;
+	// cam.options->video_height   = frameheight;
+	// cam.options->framerate      = framerate;
+	// cam.options->verbose        = verbose;
+	// cam.startVideo();
 
     startEncoders();
-	printf("%d %d\n", i2c_fd1, i2c_fd2);
-	wiringPiI2CWriteReg16(i2c_fd1, 0x82, 0xffff);
-    pthread_t Vision;
-    pthread_t Velocity;
+
+	// pthread_t Vision;
+    // pthread_t Velocity;
     pthread_t Control1;
     pthread_t Control2;
     pthread_t Control3;
-    pthread_t Motor1;
-    pthread_t Motor2;
-    pthread_t Motor3;
+    // pthread_t Motor1;
+    // pthread_t Motor2;
+    // pthread_t Motor3;
     pthread_t SpeedMonitor;
 
-	pthread_create(&Vision, NULL, computeBarycenter, NULL);
-	pthread_create(&Velocity, NULL, computeRefTPSFromVision, NULL);
+	// pthread_create(&Vision, NULL, computeBarycenter, NULL);
+	// pthread_create(&Velocity, NULL, computeRefTPSFromVision, NULL);
 	pthread_create(&Control1, NULL, controlMotor1, NULL);
 	pthread_create(&Control2, NULL, controlMotor2, NULL);
 	pthread_create(&Control3, NULL, controlMotor3, NULL);
@@ -44,8 +45,8 @@ int main(int argc, char *argv[]) {
 	// pthread_create(&Motor3, NULL, setMotor3, NULL);
 	pthread_create(&SpeedMonitor, NULL, monitorMotorsSpeed, NULL);
 
-	pthread_join(Vision, NULL);
-	pthread_join(Velocity, NULL);
+	// pthread_join(Vision, NULL);
+	// pthread_join(Velocity, NULL);
 	pthread_join(Control1, NULL);
 	pthread_join(Control2, NULL);
 	pthread_join(Control3, NULL);
