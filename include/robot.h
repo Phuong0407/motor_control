bc17288d4a21a6bd951ed17ae67980fefbdb9326
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-21 19:38:19 +0200

    .

--- include/robot.h
+++ include/robot.h
@@ -129,7 +129,7 @@ bool                    turn_left                   = false;
 bool                    turn_right                  = false;
 bool                    THROTTLE_MODE               = false;
 
-constexpr double        STUCK_THRES                 = 10.0;
+constexpr double        STUCK_THRES                 = 50.0;
 constexpr double        TURN_RIGHT_THRES            = 165.0;
 constexpr double        TURN_LEFT_THRES             = 165.0;
 
