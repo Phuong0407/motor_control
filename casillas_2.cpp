c874d22e6fa9a44bf6925136828f6af51a1f83df
Phuong0407 <diepthanhphuong0407@email.com>
2025-05-23 17:04:43 +0200

    .

--- casillas_2.cpp
+++ casillas_2.cpp
@@ -11,7 +11,7 @@
 constexpr double    BALL_DIAMETER_CM    = 4.6;
 constexpr double    DEPTH_MULTIPLIER    = 1592.06;
 constexpr int       MAX_PWM             = 255;
-
+constexpr int       DEAD_PWM            = 40;
 constexpr int       FRAME_WIDTH         = 640;
 constexpr int       FRAME_HEIGHT        = 480;
 constexpr int       FRAME_RATE          = 30;
